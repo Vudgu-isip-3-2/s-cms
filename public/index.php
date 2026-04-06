60e60a59f359c0087179746a2f8e4ca0dfb82720
xQuiets <danil_l_2007@mail.ru>
2026-04-06 10:47:11 +0400

    Добавлен Main класс #152

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
 <?php
-echo "Helow world";
+require_once __DIR__ . '/../lib/autoload.php';
+require_once __DIR__ . '/../lib/Router.php';
+require_once __DIR__ . '/../lib/ErrorHandler.php';
+require_once __DIR__ . '/../lib/Database.php';
+require_once __DIR__ . '/../lib/Config_class.php';
+require_once __DIR__ . '/../lib/Main.php';
+
+$app = new Main();
+?>
