2822f2af3c810c4cbab7a19d0fd90d1a3970b6ff
rafinadk <rafinad.uou@gmail.com>
2026-04-15 11:14:41 +0400

    routeUser

--- public/index.php
+++ public/index.php
@@ -27,6 +27,12 @@ require_once __DIR__ . '/../lib/ErrorHandler.php';
 require_once __DIR__ . '/../lib/Database.php';
 require_once __DIR__ . '/../lib/Config_class.php';
 require_once __DIR__ . '/../lib/Main.php';
-
+$router = new Router();
+$params = $router->getParams();
+foreach ($params as $key => $value) {
+    if($key == 'route'&& $value == 'user'){
+        include __DIR__ . '/../public/users.php';
+    }
+}
 $app = new Main();
 ?>
